d5939dd91c756235048c13ea5bbff834c4d0a776
loulou <loulou@sun.net.tw>
2020-12-17 08:45:24 +0800

    1.[api] 取得後台 API 的 JSON 資料 (測試撰寫) (之後須重新撰寫在 新的 component )
    1.要在 app.module 中 import 進 HttpClientModule
     * import { HttpClientModule } from '@angular/common/http';
     * HttpClientModule  需要在 BrowserModule 之後。
    
    2.在需屆接資料的 component 中撰寫程式，
     * 需在 Component 的 ngOnInit() 函式使用
     * import 進 HttpClient
         * import { HttpClient } from '@angular/common/http';
      * this.http. get <any> (url) . subscribe ( data => { this.data = data } );
     * 儲存資料是使 Array<any>
         * 但建議 使用 interface
    
    3.對應樣板顯示
     * *ngFor 內部需使用 div 包覆
    
    =======================================
    
    [討論]
    在 主m 與 新c  都是 import from @angulr/ common / http
    ==> 但其分別卻是使用 Http Client Module 與 Http Client
    ==> m 使用 m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'loulou-resume-front-end';
+
+  baseInfos:Array<any>;// 建議不要使用 any，改用 interface 比較好
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit():void{
+    const url = "http://127.0.0.1:8000/api/personalData/baseInfo";
+    this.http.get<any>(url).subscribe(baseInfos=> {
+      this.baseInfos = baseInfos;
+      console.log(baseInfos); // 印出取得 json 內容
+    });    
+  }
 }
